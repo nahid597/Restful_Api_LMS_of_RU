55e917b32949ae023fb68cab33590d9c163cc1fe
Nahid Hasan <30774439+nahid597@users.noreply.github.com>
2018-08-03 15:58:01 +0600

    schema designed.

--- api/models/books.js
+++ api/models/books.js
@@ -4,6 +4,7 @@ const bookschema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     key: { type: String, required: true },
     values: String
+    
 
     // Total_Number_of_books: String,
     // Number_of_avaiable_books: String,
